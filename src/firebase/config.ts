44b27867ff52b7664b9fe34fcf621cc79dd4b6ef
Rıdvan ÖZKAN <ridvan.ozkan.183@gmail.com>
2026-01-04 16:42:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-825719495-95028",
+  "appId": "1:844273891694:web:f2ae7015f76ec6368f297f",
+  "apiKey": "AIzaSyARHPUH3zT0esr3pDJm2NZeQ2A5LYS6RFc",
+  "authDomain": "studio-825719495-95028.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844273891694"
+};
